fc7d7618c52112d5f9b5415d1305b96b72a8732a
Md-Anikul-Islam-Binju <anikul.islam.binju@gmail.com>
2024-10-02 20:06:14 +0600

    our business site is redy to design and few things on backend

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@codernetix.com',
+            'password' => bcrypt('codernetix2645')
+        ]);
+
+    }
+}

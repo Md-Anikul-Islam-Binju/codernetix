55f2e610ace2f4629d68c9389c755a241c4a618d
Md-Anikul-Islam-Binju <anikul.islam.binju@gmail.com>
2025-09-04 11:15:40 +0600

    update

--- resources/views/frontend/pages/contact/contact.blade.php
+++ resources/views/frontend/pages/contact/contact.blade.php
@@ -5,56 +5,27 @@
         <h1 class="text-center fw-bold text-uppercase display-5 position-absolute top-50 start-50 translate-middle">
             Contact Us</h1>
     </section>
-<br><br><br>
-<!-- Contact Start -->
-<div class="container-fluid contact py-5">
-    <div class="container py-5">
-        <div class="row g-5">
-            <div class="col-xl-6">
-                <div class="wow fadeInUp" data-wow-delay="0.2s">
-                    <div class="bg-light rounded p-5 mb-5">
-                        <h4 class="text-primary mb-4">Get in Touch</h4>
-                        <div class="row g-4">
-                            <div class="col-md-6">
-                                <div class="contact-add-item">
-                                    <div class="contact-icon text-primary mb-4">
-                                        <i class="fas fa-map-marker-alt fa-2x"></i>
-                                    </div>
-                                    <div>
-                                        <h4>Address</h4>
-                                        <p class="mb-0">{{$siteSetting->address}}</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="contact-add-item">
-                                    <div class="contact-icon text-primary mb-4">
-                                        <i class="fas fa-envelope fa-2x"></i>
-                                    </div>
-                                    <div>
-                                        <h4>Mail Us</h4>
-                                        <p class="mb-0">{{$siteSetting->email}}</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="contact-add-item">
-                                    <div class="contact-icon text-primary mb-4">
-                                        <i class="fa fa-phone-alt fa-2x"></i>
-                                    </div>
-                                    <div>
-                                        <h4>Telephone</h4>
-                                        <p class="mb-0">+88{{$siteSetting->phone}}</p>
-                                    </div>
-                                </div>
-                            </div>
+
+    <!-- Contact Start -->
+    <div class="container-fluid contact bg-light py-5">
+        <div class="container py-5">
+            <div class="text-center mx-auto pb-5 wow fadeInUp" data-wow-delay="0.2s" style="max-width: 800px;">
+                <h4 class="text-primary">Contact Us</h4>
+                <h1 class="display-4 mb-4">If you have any comments please apply now</h1>
+            </div>
+            <div class="row g-5">
+                <div class="col-xl-6 wow fadeInLeft" data-wow-delay="0.2s">
+                    <div class="contact-img d-flex justify-content-center" >
+                        <div class="contact-img-inner">
+                            <img src="{{('frontend/img/contact.png')}}" class="img-fluid w-100"  alt="Image">
                         </div>
                     </div>
-                    <div class="bg-light p-5 rounded h-100 wow fadeInUp" data-wow-delay="0.2s">
+                </div>
+                <div class="col-xl-6 wow fadeInRight" data-wow-delay="0.4s">
+                    <div>
                         <h4 class="text-primary">Send Your Message</h4>
-                        <p class="mb-4">The contact form is currently inactive. Get a functional and working contact form with Ajax & PHP in a few minutes. Just copy and paste the files, add a little code and you're done. <a class="text-primary fw-bold" href="https://htmlcodex.com/contact-form">Download Now</a>.</p>
                         <form>
-                            <div class="row g-4">
+                            <div class="row g-3">
                                 <div class="col-lg-12 col-xl-6">
                                     <div class="form-floating">
                                         <input type="text" class="form-control border-0" id="name" placeholder="Your Name">
@@ -67,18 +38,13 @@
                                         <label for="email">Your Email</label>
                                     </div>
                                 </div>
-                                <div class="col-lg-12 col-xl-6">
+                                <div class="col-lg-12 col-xl-12">
                                     <div class="form-floating">
                                         <input type="phone" class="form-control border-0" id="phone" placeholder="Phone">
                                         <label for="phone">Your Phone</label>
                                     </div>
                                 </div>
-                                <div class="col-lg-12 col-xl-6">
-                                    <div class="form-floating">
-                                        <input type="text" class="form-control border-0" id="project" placeholder="Project">
-                                        <label for="project">Your Project</label>
-                                    </div>
-                                </div>
+
                                 <div class="col-12">
                                     <div class="form-floating">
                                         <input type="text" class="form-control border-0" id="subject" placeholder="Subject">
@@ -87,7 +53,7 @@
                                 </div>
                                 <div class="col-12">
                                     <div class="form-floating">
-                                        <textarea class="form-control border-0" placeholder="Leave a message here" id="message" style="height: 160px"></textarea>
+                                        <textarea class="form-control border-0" placeholder="Leave a message here" id="message" style="height: 100px"></textarea>
                                         <label for="message">Message</label>
                                     </div>
 
@@ -99,16 +65,64 @@
                         </form>
                     </div>
                 </div>
-            </div>
-            <div class="col-xl-6 wow fadeInRight" data-wow-delay="0.2s">
-                <div class="rounded h-100">
-                    <iframe class="rounded h-100 w-100"
-                            style="height: 400px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d228.20394244246273!2d90.42973009084304!3d23.773644409619113!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c78d8583ee4f%3A0x75f19c2d2c29b3e8!2zU291dGggQmFyaWRoYXJhIFJlc2lkZW50YWlsIEFyZWEsRC5JLlQuIFByb2plY3QsIOCmouCmvuCmleCmviAxMjEy!5e0!3m2!1sbn!2sbd!4v1710827452659!5m2!1sbn!2sbd" class=" w-100 h-100" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
-                            loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+                <div class="col-12">
+                    <div class="row g-4">
+                        <div class="col-md-6 col-lg-3 wow fadeInUp" data-wow-delay="0.2s">
+                            <div class="contact-add-item">
+                                <div class="contact-icon text-primary mb-4">
+                                    <i class="fas fa-map-marker-alt fa-2x"></i>
+                                </div>
+                                <div>
+                                    <h4>Address</h4>
+                                    <p class="mb-0">{{$siteSetting->address}}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-3 wow fadeInUp" data-wow-delay="0.4s">
+                            <div class="contact-add-item">
+                                <div class="contact-icon text-primary mb-4">
+                                    <i class="fas fa-envelope fa-2x"></i>
+                                </div>
+                                <div>
+                                    <h4>Mail Us</h4>
+                                    <p class="mb-0">{{$siteSetting->email}}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-3 wow fadeInUp" data-wow-delay="0.6s">
+                            <div class="contact-add-item">
+                                <div class="contact-icon text-primary mb-4">
+                                    <i class="fa fa-phone-alt fa-2x"></i>
+                                </div>
+                                <div>
+                                    <h4>Telephone</h4>
+                                    <p class="mb-0">+88{{$siteSetting->phone}}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-3 wow fadeInUp" data-wow-delay="0.8s">
+                            <div class="contact-add-item">
+                                <div class="contact-icon text-primary mb-4">
+                                    <i class="fab fa-firefox-browser fa-2x"></i>
+                                </div>
+                                <div>
+                                    <h4>Website</h4>
+                                    <p class="mb-0">{{$siteSetting->site_link}}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 wow fadeInUp" data-wow-delay="0.2s">
+                    <div class="rounded">
+                        <iframe class="rounded w-100"
+                                style="height: 400px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d228.20394244246273!2d90.42973009084304!3d23.773644409619113!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c78d8583ee4f%3A0x75f19c2d2c29b3e8!2zU291dGggQmFyaWRoYXJhIFJlc2lkZW50YWlsIEFyZWEsRC5JLlQuIFByb2plY3QsIOCmouCmvuCmleCmviAxMjEy!5e0!3m2!1sbn!2sbd!4v1710827452659!5m2!1sbn!2sbd" class=" w-100 h-100"
+                                loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 @endsection

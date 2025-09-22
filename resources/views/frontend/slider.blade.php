33be48901f351bdad28e840e45f4864afe2519bc
Md-Anikul-Islam-Binju <anikul.islam.binju@gmail.com>
2025-09-22 10:52:50 +0600

    update seo codernetix

--- resources/views/frontend/slider.blade.php
+++ resources/views/frontend/slider.blade.php
@@ -28,7 +28,7 @@
                                 <div class="d-flex justify-content-end ms-2">
                                     <a class="btn btn-md-square btn-light rounded-circle me-2" href="{{$siteSetting->facebook_link}}" target="_blank"><i class="fab fa-facebook-f"></i></a>
                                     <a class="btn btn-md-square btn-light rounded-circle mx-2" href="{{$siteSetting->twitter_link}}" target="_blank"><i class="fab fa-twitter"></i></a>
-                                    <a class="btn btn-md-square btn-light rounded-circle ms-2" href="{{$siteSetting->instagram_link}}"><i class="fab fa-linkedin-in"></i></a>
+                                    <a class="btn btn-md-square btn-light rounded-circle ms-2" href="{{$siteSetting->linkedin_link}}"><i class="fab fa-linkedin-in"></i></a>
                                 </div>
                             </div>
                         </div>

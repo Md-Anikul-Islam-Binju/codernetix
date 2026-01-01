badf3fb07d5570339d7f943d6a821b472a34962d
Md-Anikul-Islam-Binju <anikul.islam.binju@gmail.com>
2026-01-01 22:40:10 +0600

    up

--- resources/views/frontend/slider.blade.php
+++ resources/views/frontend/slider.blade.php
@@ -1,6 +1,5 @@
 @php
     $slider = DB::table('sliders')->where('status',1)->get();
-    $siteSetting = DB::table('site_settings')->first();
 @endphp
 <div class="header-carousel owl-carousel">
     @foreach($slider as $sliderData)
@@ -22,14 +21,6 @@
                                     <i class="fab fa-whatsapp me-2"></i> WhatsApp
                                 </a>
                                 <a class="btn btn-primary rounded-pill py-3 px-4 px-md-5 ms-2" href="{{route('about')}}">Learn More</a>
-                            </div>
-                            <div class="d-flex align-items-center justify-content-center justify-content-md-end">
-                                <h2 class="text-white me-2">Follow Us:</h2>
-                                <div class="d-flex justify-content-end ms-2">
-                                    <a class="btn btn-md-square btn-light rounded-circle me-2" href="{{$siteSetting->facebook_link}}" target="_blank"><i class="fab fa-facebook-f"></i></a>
-                                    <a class="btn btn-md-square btn-light rounded-circle mx-2" href="{{$siteSetting->twitter_link}}" target="_blank"><i class="fab fa-twitter"></i></a>
-                                    <a class="btn btn-md-square btn-light rounded-circle ms-2" href="{{$siteSetting->linkedin_link}}" target="_blank"><i class="fab fa-linkedin-in"></i></a>
-                                </div>
                             </div>
                         </div>
                     </div>
